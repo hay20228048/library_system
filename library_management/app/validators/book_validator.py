3139b3351d2ce98f73b5938b0d25f7c6bd10a312
hay20228048 <hay20228048@std.psut.edu.jo>
2026-02-17 11:18:14 +0300

    complete the task of Library Management System

--- library_management/app/validators/book_validator.py
+++ library_management/app/validators/book_validator.py
@@ -2,6 +2,13 @@ from pydantic import BaseModel
 from typing import Optional
 
 
+
+#logfire is used to monitor pydantic validations and understand why 
+#some inputs fails validations
+#import logfire
+#logfire.configure()
+#logfire.instrument_pydantic()  
+
 class BookCreate(BaseModel):
     title: str
     author: str
